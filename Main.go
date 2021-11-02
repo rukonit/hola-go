6a456346542ce45faa7ad9a199264bf86573c7ac
rukonit <rukon-it@live.com>
2021-11-02 01:25:16 -0400

    db operation

--- Main.go
+++ Main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/rukonit/firstapp/hola/hola2"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 
 func main() {
- fmt.Printf(hola2.Hello("Rukon"))
+
+	// db, err:= sql.Open("mysql", "rukon:123456@tcp(127.0.0.1:3306)/product")
+
+	if err != nil {
+		log.Fatal("Unable to open connection to db")
+	}
+
+	defer db.Close()
+	results, err := db.Query("select * from category")
+
+	if err != nil {
+		log.Fatal("Error occured when fetching data")
+	}
+	defer results.Close()
+
+	for results.Next() {
+		var (id int 
+		category string
+		)
+		err = results.Scan(&id, &category)
+		if err != nil {
+			log.Fatal("Unable to parse row", err)
+		}
+		fmt.Printf("ID: %d, Category: %s\n", id, category)
+	}
+	
 }
